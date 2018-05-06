dd276f38aa145808b4637cb54d242690b89641b3
Satoshi Oseki <satoshi@thetarngroup.com>
2018-05-07 10:19:18 +1200

    ContextActivities property can be either a single object or an array

---
+++ LRS/Models/Json/SingleOrArrayConverter.cs
@@ -0,0 +1,41 @@
+
+using System;
+using System.Collections.Generic;
+using System.Net.Mail;
+using bracken_lrs.Models.xAPI;
+using bracken_lrs.Services;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace bracken_lrs.Models.Json
+{
+    // https://stackoverflow.com/questions/18994685/how-to-handle-both-a-single-item-and-an-array-for-the-same-property-using-json-n
+    class SingleOrArrayConverter<T> : JsonConverter
+    {
+        public override bool CanConvert(Type objectType)
+        {
+            return (objectType == typeof(List<T>));
+        }
+
+        public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+        {
+            JToken token = JToken.Load(reader);
+            if (token.Type == JTokenType.Array)
+            {
+                return token.ToObject<List<T>>();
+            }
+            return new List<T> { token.ToObject<T>() };
+        }
+
+        public override bool CanWrite
+        {
+            get { return true; }
+        }
+
+        public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+        {
+            List<T> list = (List<T>)value;
+            serializer.Serialize(writer, value);
+        }
+    }
+}
--- LRS/Models/xAPI/ContextActivities.cs
+++ LRS/Models/xAPI/ContextActivities.cs
@@ -1,12 +1,18 @@
 ﻿using System.Collections.Generic;
+using bracken_lrs.Models.Json;
+using Newtonsoft.Json;
 
 namespace bracken_lrs.Models.xAPI
 {
     public class ContextActivities
     {
+        [JsonConverter(typeof(SingleOrArrayConverter<Activity>))]
         public IList<Activity> Parent { get; set; }
+        [JsonConverter(typeof(SingleOrArrayConverter<Activity>))]
         public IList<Activity> Grouping { get; set; }
+        [JsonConverter(typeof(SingleOrArrayConverter<Activity>))]
         public IList<Activity> Category { get; set; }
+        [JsonConverter(typeof(SingleOrArrayConverter<Activity>))]
         public IList<Activity> Other { get; set; }
     }
 }

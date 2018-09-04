4d3b14f440c3ccbe24d365d88921d9f121573a0c
Satoshi Oseki <satoshi@thetarngroup.com>
2018-09-04 13:30:50 +1200

    LRS-48: Share models between projects

--- CourseResult.ts
+++
@@ -1,9 +0,0 @@
-import { IVmQuestion } from "./IVmQuestion";
-
-export interface IVmCourseResult {
-  username: string;
-  courseId: string;
-  start: number;
-  end: number;
-  questions: IVmQuestion[];
-}

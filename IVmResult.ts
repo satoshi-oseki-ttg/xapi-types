fa51b0c60decde6399b72f979d3e2485c38af7a0
Satoshi Oseki <satoshi@thetarngroup.com>
2018-09-06 12:34:24 +1200

    LRS-47: Calculate durations instead of using values from modules

--- IVmResult.ts
+++ IVmResult.ts
@@ -2,7 +2,7 @@ export interface IVmResult {
   statementId: string;
   type: string;
   success: boolean;
-  duration: string;
+  duration: string | number; // 'PT12M1S' in xAPI, 12345 in view model as epoch
   timestamp: number;
   choices?: string[];
   response?: string;
